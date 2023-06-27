295179c6c60f147692e22d01fa35f1803c29573f
Joel Granados <j.granados@samsung.com>
2023-06-27 15:30:26 +0200

    ax.25: rm sentinel element from ctl_table array
    This is part of the greater effort to remove the empty elements
    (sentinels) from the end of the ctl_table arrays.
    
    When we remove the sentinel from ax25_param_table a buffer overflow
    shows its ugly head. The sentinel's data element used to be changed when
    CONFIG_AX25_DAMA_SLAVE was not defined. This did not have any adverse
    effects as we still stopped on the sentinel because of its null
    procname. But now that we do not have the sentinel element, we are
    carefull to check ax25_param_table's size.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- net/ax25/sysctl_net_ax25.c
+++ net/ax25/sysctl_net_ax25.c
@@ -141,8 +141,7 @@ static const struct ctl_table ax25_param_table[] = {
 		.extra2		= &max_ds_timeout
 	},
 #endif
-
-	{ }	/* that's all, folks! */
+/* that's all, folks! */
 };
 
 int ax25_register_dev_sysctl(ax25_dev *ax25_dev)
@@ -155,7 +154,7 @@ int ax25_register_dev_sysctl(ax25_dev *ax25_dev)
 	if (!table)
 		return -ENOMEM;
 
-	for (k = 0; k < AX25_MAX_VALUES; k++)
+	for (k = 0; k < AX25_MAX_VALUES && k < ARRAY_SIZE(ax25_param_table); k++)
 		table[k].data = &ax25_dev->values[k];
 
 	snprintf(path, sizeof(path), "net/ax25/%s", ax25_dev->dev->name);
